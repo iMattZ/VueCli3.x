4ec2cc22b5bbc6a2eb0439f4eb3a1a41804ca1c5
zhangbowen <mac@bogon>
2019-09-18 17:48:28 +0800

    commit

--- vue-el-admin/src/common/config/router.js
+++ vue-el-admin/src/common/config/router.js
@@ -7,16 +7,19 @@ let routes = [
 		component:'layout',
 		children:[
 			{
-				path:'/index',
-				name:'index',
+				// path:'/index',
+				// name:'index',
 				component:'index/index'
 			
+			},
+			{
+				component:'shop/goods/list'
 			}
 		]
 	},
 	{
-		path:'/login',
-		name:'login',
+		// path:'/login',
+		// name:'login',
 		component:'login/index'
 	},
 	{
@@ -36,6 +39,13 @@ let getRoutes = function(){
 function creatRoute(arr) {
 	for(let i = 0; i < arr.length;i++) {
 		if(!arr[i].component) return
+		// 去除index
+		let val = getValue(arr[i].component)
+		// 生成name
+		arr[i].name = arr[i].name || val.replace(/\//g,'_')
+		
+		// 生成path
+		arr[i].path = arr[i].path || `/${val}`
 		
 		// 自动生成component
 		let componentFun = import(`../../views/${arr[i].component}.vue`)
@@ -44,6 +54,19 @@ function creatRoute(arr) {
 			creatRoute(arr[i].children)
 		}
 	}
+}
+
+// 去除index
+function getValue(str) {
+	// 获取最后一个/到索引
+	let index = str.lastIndexOf('/')
+	// 获取最后一个/后面到值
+	let val = str.substring(index+1,str.length)
+	// 判断是不是index结尾
+	if(val === 'index') {
+		return str.substring(index,-1)
+	}
+	return str
 }
 
 export default getRoutes()	
